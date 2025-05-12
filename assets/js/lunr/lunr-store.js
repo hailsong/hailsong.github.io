7da99c13958c84cf9e45e4db70996aba0c7941f2
hailsong <hailsong@users.noreply.github.com>
2025-05-12 06:44:24 +0000

    deploy: d103771ec2b602cc5ec671fdbfdf5c9b86f9f852

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,181 @@
+var store = [{
+        "title": "LeetCode (0) - 가입하기 + 테스트 문제",
+        "excerpt":"개요 외국계 기업 재직중이신 분의 인터뷰를 유튜브에서 보다가 실리콘밸리에서는 LeetCode라는 알고리즘 문제 풀이 사이트가 우리나라의 백준, 프로그래머스 등과 같은 문제풀이와 더불어 이력으로도 남는다고 하여 신기함을 느꼈다. 심심해서 가입해보고 간단한 문제를 풀어보았다! LeetCode 가입 LeetCode의 주소는 https://leetcode.com/이다. 접속하면 다음과 같은 화면을 볼 수 있다. 우측 상단 Sign in -&gt; Sign up을...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-0/",
+        "teaser": null
+      },{
+        "title": "Upbit 비트코인 자동거래 봇 만들기 (0) - 개요 및 개발환경 (Python, Pycharm 설치)",
+        "excerpt":"개요 개인적인 투자를 위해 몇 가지 알고리즘 공부와 더불어 가상화폐 자동거래를 이용한 프로그래밍을 한 경험이 있다. 구글링을 통해 여러 거래소의 API를 이용하고, 정보를 얻는 과정에서 뿌듯함을 느꼈다. 포스트를 올리는 것은 정보 정리 목적도 있지만 언젠가는 이 주제로 인프런(링크)과 같은 강의 사이트에 강의를 올려보고 싶어서도 크다! 언젠가 시간내서 꼭 찍어야지 ㅎㅎ...","categories": ["Bitcoin Auto Trading"],
+        "tags": ["code","upbit","auto-trading","bitcoin"],
+        "url": "/bitcoin%20auto%20trading/cryptocurrency-0/",
+        "teaser": null
+      },{
+        "title": "시계열 처리 신경망 (1) - RNN",
+        "excerpt":"✅ 개요 회사 업무서 시계열 데이터를 다룰 일들이 있어 시계열 처리를 위한 신경망들을 사용했었습니다. 회사 내부적으로 AI세미나라는 이름으로 스터디를 진행하고 있는데 시계열 처리 신경망들을 발표할 일이 생겨 정리할 겸 포스트를 남겨보려 합니다. 본 포스트를 시작으로 RNN, LSTM, GRU 신경망에 대한 포스트를 업로드할 예정입니다. 발표를 위한 자료를 재정리한 정보라 순서가 조금...","categories": ["Deep Learning Theory"],
+        "tags": ["deep learning","deep learning theory","time series","RNN"],
+        "url": "/deep%20learning%20theory/deep-learning-1/",
+        "teaser": null
+      },{
+        "title": "시계열 처리 신경망 (2) - LSTM",
+        "excerpt":"✅ 개요 회사 업무서 시계열 데이터를 다룰 일들이 있어 시계열 처리를 위한 신경망들을 사용했었습니다. 회사 내부적으로 AI세미나라는 이름으로 스터디를 진행하고 있는데 시계열 처리 신경망들을 발표할 일이 생겨 정리할 겸 포스트를 남겨보려 합니다. 이 포스트는 시계열 분석 신경망 RNN, LSTM, GRU 에 대해 스터디한 내용을 정리하는 두 번째 포스트로 LSTM에 대해...","categories": ["Deep Learning Theory"],
+        "tags": ["deep learning","deep learning theory","time series","LSTM"],
+        "url": "/deep%20learning%20theory/deep-learning-2/",
+        "teaser": null
+      },{
+        "title": "시계열 처리 신경망 (3) - GRU",
+        "excerpt":"✅ 개요 회사 업무서 시계열 데이터를 다룰 일들이 있어 시계열 처리를 위한 신경망들을 사용했었습니다. 회사 내부적으로 AI세미나라는 이름으로 스터디를 진행하고 있는데 시계열 처리 신경망들을 발표할 일이 생겨 정리할 겸 포스트를 남겨보려 합니다. 이 포스트는 시계열 분석 신경망 RNN, LSTM, GRU 에 대해 스터디한 내용을 정리하는 마지막 포스트로 GRU에 대해 다룹니다....","categories": ["Deep Learning Theory"],
+        "tags": ["deep learning","deep learning theory","time series","GRU"],
+        "url": "/deep%20learning%20theory/deep-learning-3/",
+        "teaser": null
+      },{
+        "title": "LeetCode (1) - Remove Nth Node From End of List (파이썬, python)",
+        "excerpt":"❓ 문제 Given the head of a linked list, remove the nth node from the end of the list and return its head. Example 1: Input: head = [1,2,3,4,5], n = 2 Output: [1,2,3,5] Example 2: Input: head = [1], n = 1 Output: [] Example 3: Input: head =...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-1/",
+        "teaser": null
+      },{
+        "title": "LeetCode (2) - Power of three (파이썬, python)",
+        "excerpt":"❓ 문제 Given an integer n, return true if it is a power of three. Otherwise, return false. An integer n is a power of three, if there exists an integer x such that n == 3^x. Example 1: Input: n = 27 Output: true Example 2: Input: n =...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-2/",
+        "teaser": null
+      },{
+        "title": "LeetCode (3) - Shuffle an Array (파이썬, python)",
+        "excerpt":"❓ 문제 Given an integer array nums, design an algorithm to randomly shuffle the array. Implement the Solution class: Solution(int[] nums) Initializes the object with the integer array nums. int[] reset() Resets the array to its original configuration and returns it. int[] shuffle() Returns a random shuffling of the array....","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-3/",
+        "teaser": null
+      },{
+        "title": "LeetCode (4) - 3Sum (파이썬, python)",
+        "excerpt":"❓ 문제 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. Notice that the solution set must not contain duplicate triplets. Example 1: Input: nums = [-1,0,1,2,-1,-4]...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-4/",
+        "teaser": null
+      },{
+        "title": "LeetCode (5) - Longest Substring Without Repeating Characters (파이썬, python)",
+        "excerpt":"❓ 문제 Given a string s, find the length of the longest substring without repeating characters. Example 1: Input: s = \"abcabcbb\" Output: 3 Explanation: The answer is \"abc\", with the length of 3. Example 2: Input: s = \"bbbbb\" Output: 1 Explanation: The answer is \"b\", with the length...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-5/",
+        "teaser": null
+      },{
+        "title": "Data Structure (1) - BST (Binary Search Tree) 이론, 구현",
+        "excerpt":"✅ 개요 이진 탐색 트리(Binary Search Tree, BST)의 이론과 구현을 정리합니다. Priority Queue, Heap을 공부하다가 정리했습니다. https://algorithm90.tistory.com/27?category=826977 https://ratsgo.github.io/data%20structure&amp;algorithm/2017/10/22/bst/ 를 보고 공부했습니다. ❓ 이진 탐색 트리(Binary Search Tree, BST)란? 이진 탐색 트리(Binary Search Tree)는 이진 탐색(Binary search)과 연결리스트(linked list)를 결합한 자료구조의 일종입니다. cf) 이진 탐색(Binary search) : 정렬된 데이터에서 특정한 값을...","categories": ["Data Structure"],
+        "tags": ["code","Data Structure","Priority Queue","Heap"],
+        "url": "/data%20structure/DataStructure-1/",
+        "teaser": null
+      },{
+        "title": "Algorithm (1) - 다이나믹 프로그래밍 (Dynamic Programming)",
+        "excerpt":"✅ 개요 다이나믹 프로그래밍(Dynamic Programming)에 대한 이론 정리와 짧은 예제(피보나치 수열 함수) 구현 Ref : https://blog.naver.com/ndb796/221233570962 ❓ 다이나믹 프로그래밍(Dynamic Programming)이란? 하나의 문제는 단 한번만 풀도록 하는 알고리즘을 의미합니다. 큰 문제를 작은 문제로 나눌 수 있고 작은 문제에서 구한 정답이 그것을 포함하는 큰 문제에서도 동일하다면 다이나믹 프로그래밍을 사용할 수 있습니다. 예시로는...","categories": ["Algorithm"],
+        "tags": ["code","Algorithm","Dynamic Programming"],
+        "url": "/algorithm/Algorithm-1/",
+        "teaser": null
+      },{
+        "title": "Algorithm (1) - 다이나믹 프로그래밍 (Dynamic Programming)",
+        "excerpt":"✅ 개요 다이나믹 프로그래밍(Dynamic Programming)에 대한 이론 정리와 짧은 예제(피보나치 수열 함수) 구현 Ref : https://blog.naver.com/ndb796/221233570962 ❓ 다이나믹 프로그래밍(Dynamic Programming)이란? 하나의 문제는 단 한번만 풀도록 하는 알고리즘을 의미합니다. 큰 문제를 작은 문제로 나눌 수 있고 작은 문제에서 구한 정답이 그것을 포함하는 큰 문제에서도 동일하다면 다이나믹 프로그래밍을 사용할 수 있습니다. 예시로는...","categories": ["Algorithm"],
+        "tags": ["code","Algorithm","Dynamic Programming"],
+        "url": "/algorithm/etc-1/",
+        "teaser": null
+      },{
+        "title": "IT 기술면접 질문들 - (1)",
+        "excerpt":"IT 기술면접 질문들, 심심할때 정리할 예정입니다. https://mangkyu.tistory.com/88 참조했습니다! OOP (Object-Oriented Programming) (ex. java, C++, C#, python) 데이터를 객체로 취급하여 프로그램에 반영한 것으로 절차지향 프로그램이 코드 순서대로 동작하는 것과 달리 객체와 객체의 상호작용으로 동작한다. 코드의 재사용성과 개발의 생산성이 높으며 유지보수가 편리하지만 속도가 느리고 설계/개발단계에서 많은 시간이 소요된다. OOP 4가지 특성 추상화...","categories": ["Algorithm"],
+        "tags": ["Algorithm"],
+        "url": "/algorithm/IT-1/",
+        "teaser": null
+      },{
+        "title": "LeetCode (6) - Add Two Numbers (파이썬, python)",
+        "excerpt":"❓ 문제 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-6/",
+        "teaser": null
+      },{
+        "title": "LeetCode (7) -  Set Matrix Zeroes (파이썬, python)",
+        "excerpt":"❓ 문제 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0’s, and return the matrix. You must do it in place. Example 1: Input: matrix = [[1,1,1],[1,0,1],[1,1,1]] Output: [[1,0,1],[0,0,0],[1,0,1]] Example 2: Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]] Constraints:...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-7/",
+        "teaser": null
+      },{
+        "title": "LeetCode (8) -  Odd Even Linked List(파이썬, python)",
+        "excerpt":"❓ 문제 Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list. The first node is considered odd, and the second node is even, and so on. Note that the relative order...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-8/",
+        "teaser": null
+      },{
+        "title": "LeetCode (9) -  Jump Game(파이썬, python)",
+        "excerpt":"❓ 문제 You are given an integer array nums. You are initially positioned at the array’s first index, and each element in the array represents your maximum jump length at that position. Return true if you can reach the last index, or false otherwise. Example 1: Input: nums = [2,3,1,1,4]...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-9/",
+        "teaser": null
+      },{
+        "title": "LeetCode (10) -  Coin Change(파이썬, python)",
+        "excerpt":"❓ 문제 You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money. Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-10/",
+        "teaser": null
+      },{
+        "title": "LeetCode (11) -  Longest Increasing Subsequence(파이썬, python)",
+        "excerpt":"❓ 문제 Given an integer array nums, return the length of the longest strictly increasing subsequence. A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-11/",
+        "teaser": null
+      },{
+        "title": "LeetCode (12) - Happy Number(파이썬, python)",
+        "excerpt":"❓ 문제 Write an algorithm to determine if a number n is happy. A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits. Repeat the process until the number equals 1 (where...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-12/",
+        "teaser": null
+      },{
+        "title": "LeetCode (13) - Sqrt(x)(파이썬, python)",
+        "excerpt":"❓ 문제 Given a non-negative integer x, compute and return the square root of x. Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned. Note: You are not allowed to use any built-in exponent function or operator,...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-13/",
+        "teaser": null
+      },{
+        "title": "논문 읽기(1) - Body2Hands: Learning to Infer 3D Hands from Conversational Gesture Body Dynamics (CVPR 2021)",
+        "excerpt":"논문 읽기 근본없이 필요한 부분만 공부하고 사용하는 모습이 무서워져 논문 읽기 연습을 조금씩 해보는 겸 정리를 해보고자 한다! 무지한 기계과 학부 졸업생이 나름대로 읽은 내용을 정리하는 것인지라 많이 부족할텐데 혹시 잘못되거나 표현이 이상한 부분이 있다면 피드백 환영합니다. 😁 개요 Body2Hands: Learning to Infer 3D Hands from Conversational Gesture Body Dynamics...","categories": ["paper review"],
+        "tags": ["deep learning","hand gesture","GAN","CVPR"],
+        "url": "/paper%20review/PaperReview-1/",
+        "teaser": null
+      },{
+        "title": "LeetCode (14) - Binary Tree Inorder Traversal(파이썬, python)",
+        "excerpt":"❓ 문제 Given the root of a binary tree, return the inorder traversal of its nodes’ values. Example 1: Input: root = [1,null,2,3] Output: [1,3,2] Example 2: Input: root = [] Output: [] Example 3: Input: root = [1] Output: [1] Example 4: Input: root = [1,2] Output: [2,1] Example...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-14/",
+        "teaser": null
+      },{
+        "title": "LeetCode (15) - Binary Tree Zigzag Level Order Traversal(파이썬, python)",
+        "excerpt":"❓ 문제 Given the root of a binary tree, return the zigzag level order traversal of its nodes’ values. (i.e., from left to right, then right to left for the next level and alternate between). Example 1: Input: root = [3,9,20,null,null,15,7] Output: [[3],[20,9],[15,7]] Example 2: Input: root = [1] Output:...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-15/",
+        "teaser": null
+      },{
+        "title": "LeetCode (16) - Construct Binary Tree from Preorder and Inorder Traversal(파이썬, python)",
+        "excerpt":"❓ 문제 Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree. Example 1: Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7] Output: [3,9,20,null,null,15,7] Example 2: Input: preorder...","categories": ["Algorithm - LeetCode"],
+        "tags": ["code","algorithm"],
+        "url": "/algorithm%20-%20leetcode/LeetCode-16/",
+        "teaser": null
+      },{
+        "title": "Probability and Statistics (1) - Summary (CH.2 ~ CH.7)",
+        "excerpt":"Summary for mid term of CC511, Probability and Statistics           CH.2                  CH.3                  CH.4                  CH.5                  CH.7             ","categories": ["Coursework - 2022-1"],
+        "tags": ["coursework","cc511","statistics","probability"],
+        "url": "/coursework%20-%202022-1/Probability-and-Statistics-Summary/",
+        "teaser": null
+      },{
+        "title": "Run SCANimate (CVPR 2021)",
+        "excerpt":"SCANimate SCANimate: Weakly Supervised Learning of Skinned Clothed Avatar Networks (CVPR 2021 Oral) SCANimate Project Page SCANimate는 3D Human model generation에 대해 깊고 다양한 연구를 하는 Max-Planck-Gesellschaft 에서 연구된 논문으로 Weakly Supervised Learning을 통해 구멍이나 부족한 부분이 있는 여러 Raw Scan Data를 통해 얻은 Input Data로 옷을 포함한 상태의 Animatable...","categories": ["Avatar"],
+        "tags": ["Avatar","Deep Learning"],
+        "url": "/avatar/Run-SCANimate-1/",
+        "teaser": null
+      },{
+        "title": "Run SMPLify-X (CVPR 2019)",
+        "excerpt":"SMPLify-X demo code 돌리는데에 시행착오가 조금 있었어서 저같은 상황에서 참조하실 분들이 계실까봐 포스트로 남깁니다… 개발환경은 ubuntu18.04, cuda 10.0 이었다. https://github.com/vchoutas/smplify-x/issues/3#issuecomment-507682801 https://colab.research.google.com/github/sergeyprokudin/smplpix/blob/main/colab_notebooks/Convert_Video_to_SMPLpix_Dataset.ipynb 첫번째 링크에서 conda로 환경구성하는데에 오류가 있다는 이슈가 있다는 이야기가 있어서 따라 해봤는데 결과가 좋지 않았다. 두번째 링크는 SMPLpix라는 SMPL모델을 Neural rendering하는 논문(자세히는 아직 보지 못했다.)의 과정에서 SMPLify-x를 쓰는 코드가...","categories": ["Avatar"],
+        "tags": ["Avatar","Deep Learning","Running Code"],
+        "url": "/avatar/Run-SMPLify-x-1/",
+        "teaser": null
+      },{
+        "title": "Run 3D Gaussian Splatting (1) - Windows에서 3D 가우시안 스플래팅 구동",
+        "excerpt":"3D Gaussian Splatting (Windows) Getting Started With 3D Gaussian Splatting for Windows (Beginner Tutorial) Scene Representation에서 최근 엄청난 성능 (특히 렌더링 속도 측면)을 보인 3D Gaussian Splatting에 대해서 윈도우에서 구동되는 좋은 예제가 위 링크에 소개되어 있어 정리하는 겸 한국어로 포스트를 남기게 되었습니다. Container 따로 서버에 만들고 할 필요없이 테스트 용으로...","categories": ["Study"],
+        "tags": ["Study","Gaussian Splating","Running Code"],
+        "url": "/study/Gaussian-Splatting-1/",
+        "teaser": null
+      }]
